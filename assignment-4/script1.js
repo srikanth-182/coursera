58998c8b2c895e3e12b7c0475d37a71c82754b26
srikanth-182 <81480156+srikanth-182@users.noreply.github.com>
2021-03-29 12:42:14 +0530

    Update script1.js

--- assignment-4/script1.js
+++ assignment-4/script1.js
@@ -1,18 +1,18 @@
 var names = new Array();
-names[0] = "sri";
-names[1] = "jhon";
-names[2] = "loki"
-names[3] = "ali";
-names[4] = "rohan";
-names[5] = "jessy";
-names[6] = "frank";
-names[7] = "Jhosena";
-names[8] = "alia";
-names[9] = "Jasmin";
+names[0] = "yaakov";
+names[1] = "john";
+names[2] = "jen"
+names[3] = "janson";
+names[4] = "Paul";
+names[5] = "Frank";
+names[6] = "Larry";
+names[7] = "Paula";
+names[8] = "Laura";
+names[9] = "Jim";
 
 for (var i = 0;i<names.length; i++){
 	if(names[i].charAt(0) === "J" || names[i].charAt(0) ==="j"){
-		console.log("Goodbye "+names[i])
+		console.log("Good Bye "+names[i])
 	}
 	else{
 		console.log("Hello "+names[i])
